9745833a5ea4808b0ab45c4baa182d9526535648
Alyson <alyson.subrim2002@gmail.com>
2023-02-02 18:40:34 -0300

    fix errors

--- tests/cars.test.ts
+++ tests/cars.test.ts
@@ -2,10 +2,15 @@ import { cars } from "@prisma/client";
 import { date, number, string } from "joi";
 import supertest from "supertest";
 import app from "../src";
+import prisma from "../src/config/database";
 
 const server = supertest(app);
 
 describe("cars basic tests", () => {
+
+    beforeEach(async () => {
+        await prisma.$executeRaw`TRUNCATE TABLE cars;`;
+    });
 
     it("specif cars return 200", async () => {
         const result = await server.get("/cars");
@@ -13,8 +18,17 @@ describe("cars basic tests", () => {
     })
 
     it("specif cars body", async () => {
-        const result = await server.get("/cars/1");
-        expect(result.body).toEqual({
+        const car: Omit<cars, "id" | "createAt"> = {
+            model: "civic",
+            licensePlate: "7777777",
+            year: "2020",
+            color: "red"
+        }
+
+        const inserted = await server.post("/cars").send(car);
+
+        const result = await server.get("/cars");
+        expect(result.body[0]).toEqual({
             color: expect.any(String),
             createAt: expect.any(String),
             id: expect.any(Number),
@@ -51,4 +65,8 @@ describe("cars basic tests", () => {
 
         expect(result.statusCode).toEqual(422);
     })
+
+    afterAll(async () => {
+        await prisma.$disconnect();
+    });
 });
